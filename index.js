994e98ee4f953df1abdb4f73b4f975bcc0025e43
Sonja <sonjaljatkovic@gmail.com>
2023-10-19 11:24:46 -0400

    added validate functions for colors

--- index.js
+++ index.js
@@ -12,13 +12,20 @@ const questions = [
     {
         type: "input",
         name: "shapeColor",
-        message: "What color do you want your logo shape to be? \nPlease enter color name or a hexadecimal number."
-        
+        message: "What color do you want your logo shape to be? \nPlease enter color name or a hexadecimal number.",
+        validate: userShapeColorInput => {
+            if (userShapeColorInput) {
+                return true;
+            } else {
+                console.log ("\nPlease enter a color name or color code.")
+                return false;
+            }
+        }
     },
     {
         type: "input",
         name: "text",
-        message: "Please enter up to three characters for the logo text.",
+        message: "Please enter up to three characters for the logo text. Keep in mind that input is case sensitive.",
         validate: userTextInput => {
             if (userTextInput.length < 4 && userTextInput.length > 0) {
                 return true;
@@ -31,7 +38,15 @@ const questions = [
     {
         type: "input",
         name: "textColor",
-        message: "What color do you want your logo text to be?  \nPlease enter color name or a hexadecimal number."
+        message: "What color do you want your logo text to be?  \nPlease enter color name or a hexadecimal number.",
+        validate: userTextColorInput => {
+            if (userTextColorInput) {
+                return true;
+            } else {
+                console.log ("\nPlease enter a color name or color code.")
+                return false;
+            }
+        }
     }
 ];
 
@@ -43,7 +58,7 @@ function renderLogo(fileName, userInput) {
         if (error) {
             return console.log(error);
     }
-    console.log("Generated logo.svg");
+    console.log("Generated logo.svg\nIf your logo doesn't look how you thought it would, please check if you entered valid color names and hexadecimal numbers.");
     })
 };
 
